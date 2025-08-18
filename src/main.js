5818c7f597322b2fd30b918b9078b49bf9f517e2
Darios2021 <perezruartedario@gmail.com>
2025-08-18 17:58:23 -0300

    asd2141245

--- src/main.js
+++ src/main.js
@@ -8,6 +8,19 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({ components, directives })
+// Íconos MDI
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
 
 createApp(App).use(router).use(vuetify).mount('#app')
